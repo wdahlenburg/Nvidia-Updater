5bccc023ddb5eb5f87d493a84b9182cd69dad2ce
Wyatt Dahlenburg <wddahlenburg@gmail.com>
2018-08-25 17:47:25 -0500

    Nvidia disabled https. Script was updated to force the url to https

--- nvidia-updater.sh
+++ nvidia-updater.sh
@@ -15,6 +15,11 @@ fi
 
 TARGET_URL=$(curl -s "https://www.nvidia.com/Download/processDriver.aspx?psid=101&pfid=815&rpf=1&osid=12&lid=1&lang=en-us&ctk=0");
 
+if [[ ! $TARGET_URL = *"https://"* ]]; then
+  echo "Missing https"
+  TARGET_URL=$(printf "https:"; cut -d':' -f2 <<<"$TARGET_URL")
+fi
+
 NVIDIA_NEWEST_VERSION=$(curl -s $TARGET_URL |  grep -A1 -w tdVersion | tail -n1 | awk '{$1=$1};1' | tr -d $'\r');
 echo "Nvidia's current version is $NVIDIA_NEWEST_VERSION";
 
@@ -25,6 +30,7 @@ else
 	exit;
 fi
 
+echo "Downloading driver"
 wget http://us.download.nvidia.com/XFree86/Linux-x86_64/$NVIDIA_NEWEST_VERSION/NVIDIA-Linux-x86_64-$NVIDIA_NEWEST_VERSION.run -O ~/Downloads/NVIDIA-Linux-x86_64-$NVIDIA_NEWEST_VERSION.run -q --show-progress
 
 echo "Stopping X-Server";
